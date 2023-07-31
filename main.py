ed649ae721f0e04ea9474bd909a836099a5a21b4
MaryamDV <140394282+MaryamDV@users.noreply.github.com>
2023-07-31 13:46:33 +0300

    добавили в дискорд
    теперь программа работает в дискорде

--- main.py
+++ main.py
@@ -1,14 +1,43 @@
+import discord
 import random
-list_numbers = "1234567890"
-list_letter = "qwertyuiopasdfghjklzxcvbnm"
-list_letter = list_letter + list_letter.upper()
-list_symbols = "!@#$%^&*()?|/~"
-list_all = list_numbers + list_letter + list_symbols
-pas_len = int(input("Какой длинны хотите пароль?"))
+from discord.ext import commands
+# Переменная intents - хранит привилегии бота
+intents = discord.Intents.default()
+# Включаем привелегию на чтение сообщений
+intents.message_content = True
+# Создаем бота в переменной client и передаем все привелегии
+bot = commands.Bot(intents=intents,command_prefix="!")
 
-for j in range (10):
+def generate(pas_len):
+    list_numbers = "1234567890"
+    list_letter = "qwertyuiopasdfghjklzxcvbnm"
+    list_letter = list_letter + list_letter.upper()
+    list_symbols = "!@#$%^&*()?|/~"
+    list_all = list_numbers + list_letter + list_symbols
+
     password = ""
     for i in range(pas_len):
         password += random.choice(list_all)
-    print("Ваш пароль:", password)
+    return password
+        
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Привет,я бот и умею генирировать пароли)\nВоспользуйтесь командой :\n!password")
     
+@bot.command()
+async def password(ctx, pas_len = 8):
+    text = "Ваш пароль :"+generate(pas_len)
+    await ctx.send(text)
+    
+    
+    
+    
+    
+    
+    
+bot.run("MTEzNDQyODg5OTQ4NTQzMzk3Ng.GgsjIo.vUnYE_ZDrQ90J53sx5XeV_nNv-A4SYYzcwGsjM")
